12c10ce5d7bd74012dea1c0d4b47c98e96c9094b
Emery Trinidad <72650845+trinidademery@users.noreply.github.com>
2020-10-17 20:01:57 +0800

    Pinalitan ko po ng for(j = 24-1; j>= i; j--) ung for(j = i; j >= 0; j --)

---
+++ TRIANGLE.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+ int i,j,k;
+ clrscr();
+
+ for(i = 0; i < 24; i++)
+ {
+  for(j = 24-1; j >= i; j--)
+  {
+  printf(" ");
+  }
+  for(k = 0; k <= i; k++)
+  printf("*");
+  printf("\n");
+ }
+
+ getch();
+}
